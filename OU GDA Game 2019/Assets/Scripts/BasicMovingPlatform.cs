c499df499f387219e984b5c31dcad5794dbc7340
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2019-06-27 01:04:54 -0500

    Changed to RigidBodies lmaaoooo

---
+++ OU GDA Game 2019/Assets/Scripts/BasicMovingPlatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovingPlatform : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+    public float moveSpeed = 1;
+    private GameObject target = null;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = null;
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(startPos, endPos, Mathf.Clamp01(Mathf.Sin(Time.time) * moveSpeed));
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        col.transform.parent = transform;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        col.transform.parent = null;
+    }
+
+}
---
+++ OU GDA Game 2019/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float jumpStrength = 5;
+    public float moveSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(Vector3.right * Input.GetAxisRaw("Horizontal") * moveSpeed, ForceMode.Force);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+        }
+
+    }
+}
