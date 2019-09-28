0f99df80c906b7ec3f5a0de71a7b1599515edc73
Noah Zemlin <noah.zemlin@gmail.com>
2019-09-27 21:31:49 -0500

    Lesson 2 Files

---
+++ Fall2019Game/Assets/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject[] waypoints;    int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        transform.position = waypoints[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, waypoints[index].transform.position) < .1)
+        {
+            index = index + 1;
+            if (index > waypoints.Length - 1)
+            {
+                System.Array.Reverse(waypoints);
+                index = 1;
+            }
+        }
+        else
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[index].transform.position, 0.05f);    }
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = this.transform;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+
+}
---
+++ Fall2019Game/Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed = 10;
+    public float jumpSpeed = 5;
+    float yaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        Cursor.visible = false; //Press ESC to make visible
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isGrounded());
+
+        yaw += Input.GetAxis("Mouse X") * 5;
+        transform.eulerAngles = new Vector3(0, yaw, 0);
+
+        Vector3 velocity = new Vector3(0,0,0);
+        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        velocity.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) {
+            this.rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+        }
+
+        if (rb.velocity.y < 1)
+        {
+            this.rb.AddForce(new Vector3(0, -15, 0));
+        }
+
+        this.transform.Translate(velocity);
+    }
+    bool isGrounded()
+    {
+        //Shoot a ray down from center of player to see if we are touching the ground
+        //Length of ray is height from center of player to bottom of player (0.5 in this case) + 0.1 to reach the ground
+        return Physics.Raycast(this.transform.position, Vector3.down, 1.1f);
+    }
+}
